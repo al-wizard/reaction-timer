d7a1ae9d7b733dca355dc921d2689d1fba0bc089
Alex Gayduk <alex.gayduk@gmail.com>
2021-02-10 16:43:46 +0200

    Проект сделан

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <h3 class="info">Total score: <span class="big-numbers">{{ totalScore }}</span></h3>
+  <button @click="start" :disabled="isPlaying">START</button>
+  <button @click="clear">RESET</button>
+  <p v-if="delay !=null">
+        <small>
+            Квадратик появится через: {{ delay }} милисекунд!
+        </small>
+  </p>
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" /> 
+  <Results v-if="showResults" :score="score" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+      totalScore: 0
+    }
+  },
+  methods: {
+    start(){
+      this.delay = Math.round(2000 + Math.random() * 4000)
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+      this.totalScore += this.score
+    },
+    clear() {
+      this.showResults = false
+      this.score = null
+      this.isPlaying = false
+      this.delay = null
+      this.total.score = 0
+    }
   }
 }
 </script>
@@ -20,7 +59,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+.info {
+  color: rgb(31, 121, 148)
+}
+button {
+   min-width: 200px;
+   background: #0faf87;
+   color: white;
+   border: none;
+   border-radius: 5px;
+   padding: 9px 16px;
+   font-size: 16px;
+   letter-spacing: 1px;
+   cursor: pointer;
+   margin-right: 5px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+.big-numbers {
+  font-size: 46px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="results">
+      <h3 :class="classs">Реакция: {{ score }} милисекунд!</h3>
+      <p :class="classs">Вердикт: {{ rank }} </p>        
+    </div>
+
+</template>
+
+<script>
+   export default {
+       props: ['score'],
+       data() {
+           return {
+               rank: null,
+               classs: null
+           }
+       },
+       mounted() {
+           if(this.score < 350) {
+               this.rank = 'пальцы ниндзи..!'
+               this.classs = 'green'
+           } else if(this.score < 400) {
+               this.rank ='хорошие рефлексы!'
+               this.classs = 'brown'
+           } else {
+               this.rank = 'медленновато будет...'
+               this.classs = 'red'
+           }
+       }
+   }
+    
+</script>
+
+<style scoped>
+    .results {
+        border: 1px dotted grey;
+        border-radius: 10px;
+        padding: 15px;
+    } 
+    .green { color: green }
+    .brown { color: brown }
+    .red { color: red }        
+</style>
